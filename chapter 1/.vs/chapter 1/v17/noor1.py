c360b8f8a23718f92005e72e86a06ad35d51198e
Nooryaser2016 <nooor2016.yaser@gmail.com>
2025-04-23 17:30:10 +0400

    hyftfgytfygftf

---
+++ chapter 1/.vs/chapter 1/v17/noor1.py
@@ -0,0 +1,12 @@
+a=100.890
+print(type(a))
+n="the best"
+print(type(n))
+noor=86759
+print(type(noor))
+Hafedh=True
+print(type(Hafedh))
+print(int(a))
+a=int(a)
+print(type(a))
+
---
+++ chapter 1/.vs/chapter 1/v17/nooristhebest.py
@@ -0,0 +1,3 @@
+a=int(input("inter the first number "))
+b=int(input("inter the second number "))
+print(a+b)
