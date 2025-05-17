a30d6b3d47103649dd053d0df99aea543fb48e1e
Nooryaser2016 <nooor2016.yaser@gmail.com>
2025-05-18 00:32:24 +0400

    check age

---
+++ todays chapeter/check age.py
@@ -0,0 +1,9 @@
+age = int(input('enter your age'))
+
+if age >= 10:
+    if age <= 20:
+        print('your age between 10 and 20')
+    else:
+        print('your age is above 20')
+else:
+    print('your age is below 10')
