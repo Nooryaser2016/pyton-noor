65b77ae125d806aac1cf2cb2f6cd5bd5d1dd6961
Nooryaser2016 <nooor2016.yaser@gmail.com>
2025-05-21 18:27:05 +0400

    hii

---
+++ hafedh 2 project/hafedh2.py
@@ -0,0 +1,16 @@
+# four varilbles
+x = 15
+d = "hafedh"
+f = 25.5
+print(x)
+print(type(x))
+print(d)
+print(type(d))
+print(f)
+print(type(f))
+f = int(f)
+print(type(f))
+print(f)
+print(len(d))
+print(d[2])
+print(d[0:2])
