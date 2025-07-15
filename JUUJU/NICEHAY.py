476515a858d71a8293035ce0a9513c784c5346bc
Nooryaser2016 <nooor2016.yaser@gmail.com>
2025-07-15 04:32:11 +0400

    120

---
+++ JUUJU/NICEHAY.py
@@ -0,0 +1,16 @@
+
+class Robot:
+    def __init__(self, name, model):
+        self.name = name
+        self.model = model
+
+def introduce(self):
+    print(f"Hello! My name is {self.name}. I am a {self.model} model robot.")
+
+
+robot1 = Robot("Tom", "TX-100")
+robot2 = Robot("Jerry", "JX-200")
+
+
+robot1.introduce()
+robot2.introduce()
