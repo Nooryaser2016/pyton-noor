020c02cc4c16f4f596eab8ad0b0a05e62e930ec1
Nooryaser2016 <nooor2016.yaser@gmail.com>
2025-07-29 04:07:54 +0400

    hii

---
+++ nive02/pyhomev.py
@@ -0,0 +1,10 @@
+file_name = "students.txt"
+
+# Write data
+with open(file_name, 'a') as f:
+    f.write("Roy - Math\n")
+    f.write("Anna - Science\n")
+
+# Read data
+with open(file_name, 'r') as f:
+    print(f.read())
