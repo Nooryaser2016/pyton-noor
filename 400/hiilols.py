eb649283308fba282fb2ef4065595468a2719412
Nooryaser2016 <nooor2016.yaser@gmail.com>
2025-08-12 19:28:16 +0400

    hii

---
+++ 400/hiilols.py
@@ -0,0 +1,23 @@
+
+from tkinter import messagebox
+from PIL import Image, ImageTk
+
+# Setting up Main Window
+root = Tk()
+root.title('Denomination Counter')
+root.configure(bg='light blue')
+root.geometry('650x400')
+
+
+upload = Image.open("download.jpg")
+# Resize the image using resize() method
+upload = upload.resize((300, 300))
+image = ImageTk.PhotoImage(upload)
+label = Label(root, image=image, bg='light blue')
+label.place(x=180, y=20)
+label1 = Label(root, text="Hello welcome to dinomination calculation app")
+button = Button(root, text ="lets get started")
+
+
+
+
