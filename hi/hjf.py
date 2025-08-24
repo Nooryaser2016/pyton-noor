12d37f8786fd3ddf2433ca17cd71479d3d451eb8
Nooryaser2016 <nooor2016.yaser@gmail.com>
2025-08-25 02:12:11 +0400

    jfs

---
+++ hi/hjf.py
@@ -0,0 +1,13 @@
+def myFunction1(n):
+    if (n <= 0):
+        return
+for i in range(0, n+1):
+    print("Codingal1")
+myFunction1(n//2)
+myFunction1(n//3)   
+
+def myFunction2(n):
+    if (n <= 1):
+        return
+print("Codingal2")
+myFunction2(n-1)
