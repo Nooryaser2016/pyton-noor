1a69751b193ff0f6a4e3a1c6b39cd16262d20f2b
Nooryaser2016 <nooor2016.yaser@gmail.com>
2025-09-30 19:25:44 +0400

    hii

---
+++ اههههه/bit2.py/bit27.py
@@ -0,0 +1,15 @@
+import math;
+def printPowerSet(set,SetSize):
+    PowerSetSize = (int) (math.pow(2,SetSize));
+outer = 0
+inner = 0
+
+for outer in range(0,PowerSetSize):
+    for inner in range(0,SetSize):
+
+inner element from set
+
+
+
+
+    
