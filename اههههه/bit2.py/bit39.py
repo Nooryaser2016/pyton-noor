84c012ef287383986b7c4257d78cbac2cffd8a7e
Nooryaser2016 <nooor2016.yaser@gmail.com>
2025-10-15 18:21:20 +0400

    huiii

---
+++ اههههه/bit2.py/bit39.py
@@ -0,0 +1,11 @@
+def ways(stairs):
+    if stairs==0:
+        return 1
+    twosteps = 0
+    onestep = 0
+    if (stairs>=2):
+        twosteps = ways(stairs-2)
+    onestep = ways(stairs-1)
+    return twosteps+onestep
+stairs = int(input("Enter number of steps  "))
+print("Number to ways to climb  ",ways(stairs))
