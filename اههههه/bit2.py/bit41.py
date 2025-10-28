e4a261a74c579665e0f5b3e3dd57d45d057d42c3
Nooryaser2016 <nooor2016.yaser@gmail.com>
2025-10-28 19:21:40 +0400

    hi

---
+++ اههههه/bit2.py/bit41.py
@@ -0,0 +1,9 @@
+def hanoi(n , a , b , c):
+    if n == 1:
+        print(" Move disk 1 from rod ", a ,"to rod", b)
+        return
+    hanoi(n-1, a , c , b)
+    print("move desk",n,"from rod",a,"to rod",b)
+    hanoi(n-1,c,b,a) 
+n = 4
+hanoi(n,'A','C','B')
