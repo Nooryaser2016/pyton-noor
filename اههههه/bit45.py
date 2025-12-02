3359913f624b50e3fe8eb829599271acdd25e915
Nooryaser2016 <nooor2016.yaser@gmail.com>
2025-12-02 19:19:51 +0400

    hii

---
+++ اههههه/bit45.py
@@ -0,0 +1,20 @@
+def reverse(a,a_size,n):
+    temp = 0
+    while(temp<a_size):
+        start = temp
+        end = min(temp+n-1,a_size-1)
+        while (start < end) :
+                 a[start],a[end]=a[end],a[start]
+                 start += 1
+                 end -= 1
+        temp += n 
+a = [5,4,2,7,9,13,45,22,34,56]
+n = 2
+a_size = len(a)
+reverse(a,a_size,n)
+
+for i in range(0,a_size):
+    print(a[i],end =" ")
+
+
+    
